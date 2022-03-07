3e75911953c7e6c035ade4cb0dc3075262e27c4b
sasakiyudai <yudai14142@gmail.com>
2022-03-08 00:00:59 +0900

    move /profiles/:username to anonymous group

--- api/main.go
+++ api/main.go
@@ -62,14 +62,15 @@ func main() {
 
 	v1 := r.Group("/api")
 	users.UsersRegister(v1.Group("/users"))
+	
 	v1.Use(users.AuthMiddleware(false))
+	users.ProfileAnonymousRegister(v1.Group("/profiles"))
 	articles.ArticlesAnonymousRegister(v1.Group("/articles"))
 	articles.TagsAnonymousRegister(v1.Group("/tags"))
 
 	v1.Use(users.AuthMiddleware(true))
 	users.UserRegister(v1.Group("/user"))
 	users.ProfileRegister(v1.Group("/profiles"))
-
 	articles.ArticlesRegister(v1.Group("/articles"))
 
 	testAuth := r.Group("/api/ping")
